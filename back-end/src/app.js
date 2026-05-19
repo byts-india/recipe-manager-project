dec42cf6c7fef68723618d2d5c59bd89ce944914
Avi <avinashkumar@mviiitech.com>
2026-05-19 14:26:10 +0530

    updated

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,15 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const authRoute = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
-app.use(cors({ origin: "*" })); 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.get("/health", (req,res) => {
-    res.send("SERVER IS RUNNING HEALTHY 💊")
+app.get("/health", (req, res) => {
+  res.send("SERVER IS RUNNING HEALTHY 💊");
 });
+
+app.use("/auth", authRoute);
 
 module.exports = app;
