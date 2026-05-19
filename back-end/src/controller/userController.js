31fd4668f6a147b79ea067a0f2cae38a3e66aaee
Avi <avinashkumar@mviiitech.com>
2026-05-19 14:17:39 +0530

    updated

--- back-end/src/controller/userController.js
+++ back-end/src/controller/userController.js
@@ -8,10 +8,16 @@ module.exports.register = async (req, res) => {
     successResponse(res, "new user got created", null, 201);
   } catch (error) {
     console.log(error);
-    failureResponse(res,error.message, 401);
+    failureResponse(res, error.message, 401);
   }
 };
 module.exports.login = async (req, res) => {
   try {
-  } catch (error) {}
+    const { email, password } = req.body;
+    await userService.login(email, password);
+    successResponse(res, "login is valid", null, 200);
+  } catch (error) {
+    console.log(error);
+    failureResponse(res, error.message, 401);
+  }
 };
